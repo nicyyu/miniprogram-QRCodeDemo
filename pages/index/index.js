dcbdaf9e6d655dfafc1cda82c38377ea70382345
nicyyu <nicholas.yyu@outlook.com>
2021-02-01 16:03:20 -0800

    made some changes

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,146 @@
-// index.js
-// 获取应用实例
-const app = getApp()
+import drawQrcode from '../../utils/weapp.qrcode.js'
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    text: 'https://m.baidu.com',
+    inputValue: ''
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onLoad () {
+    this.draw()
+  },
+  changeText (text) {
+    if (!this.data.inputValue) {
+      wx.showModal({
+        title: '提示',
+        content: '请先输入要转换的内容！',
+        showCancel: false
+      })
+      return
+    }
+    this.setData({
+      text: this.data.inputValue
+    })
+    this.draw()
+  },
+  bindKeyInput (e) {
+    this.setData({
+      inputValue: e.detail.value
     })
   },
-  onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  draw () {
+    drawQrcode({
+      width: 160,
+      height: 160,
+      x: 20,
+      y: 20,
+      canvasId: 'myQrcode',
+      // ctx: wx.createCanvasContext('myQrcode'),
+      typeNumber: 10,
+      text: this.data.text,
+      image: {
+        imageResource: '../../images/logo.png',
+        dx: 70,
+        dy: 70,
+        dWidth: 60,
+        dHeight: 60
+      },
+      callback(e) {
+        console.log('e: ', e)
+      }
+    })
+  },
+  repaint () {
+    // 设置二维码起始位置 x,y
+    drawQrcode({
+      width: 160,
+      height: 160,
+      x: 20,
+      y: 20,
+      canvasId: 'myQrcode',
+      typeNumber: 10,
+      text: this.data.text,
+      callback(e) {
+        console.log('e: ', e)
+      }
+    })
+  },
+  download () {
+    // 导出图片
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      width: 300,
+      height: 300,
+      destWidth: 300,
+      destHeight: 300,
+      canvasId: 'myQrcode',
+      success(res) {
+        console.log('图片的临时路径为：', res.tempFilePath)
+        let tempFilePath = res.tempFilePath
+        // 保存图片，获取地址
+        // wx.saveFile({
+        //   tempFilePath,
+        //   success (res) {
+        //     const savedFilePath = res.savedFilePath
+        //     console.log('savedFilePath', savedFilePath)
+        //   }
+        // })
+
+        // 保存到相册
+        wx.saveImageToPhotosAlbum({
+          filePath: tempFilePath,
+          success: function (res) {
+            wx.showToast({
+              title: '保存成功',
+              icon: 'success',
+              duration: 2000
+            })
+          },
+          fail: function (res) {
+            wx.showToast({
+              title: '保存失败',
+              icon: 'none',
+              duration: 2000
+            })
+          }
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    })
   },
-  getUserInfo(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  getBase64Data () {
+    // 获取二维码 base64 格式
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      width: 300,
+      height: 300,
+      destWidth: 300,
+      destHeight: 300,
+      canvasId: 'myQrcode',
+      success(res) {
+        console.log('图片的临时路径为：', res.tempFilePath)
+        let tempFilePath = res.tempFilePath
+        // 获取 base64
+        wx.getFileSystemManager().readFile({
+          filePath: tempFilePath,
+          encoding: 'base64',
+          success: function (res) {
+            console.log('[base64 data is]', res)
+            wx.showToast({
+              title: '获取成功',
+              icon: 'success',
+              duration: 2000
+            })
+          },
+          fail: function (res) {
+            wx.showToast({
+              title: '获取失败',
+              icon: 'none',
+              duration: 2000
+            })
+          }
+        })
+      }
     })
   }
 })
